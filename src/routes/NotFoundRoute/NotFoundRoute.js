04737e3532c65baac3c03138b70c36ba552b43d6
wavinginspace <paulbaisley@gmail.com>
2020-04-17 16:03:03 -0400

    styled 404 page

--- src/routes/NotFoundRoute/NotFoundRoute.js
+++ src/routes/NotFoundRoute/NotFoundRoute.js
@@ -1,14 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './NotFoundRoute.scss';
 
 class NotFoundRoute extends Component {
   render() {
     return (
-      <section>
-        <h2>404 - Page not found</h2>
-        <p>Try going back to your previous page.</p>
+      <section className="error-page">
+        <h2 className="error-page-header"><span className="error-header-highlight">404</span> - Page not found</h2>
+        <p className="error-page-message">
+          Try going back to your previous page.
+        </p>
       </section>
     );
   }
 }
 
-export default NotFoundRoute
+export default NotFoundRoute;
